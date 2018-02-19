a70b5bb6b6e8e5a7e18a3f0b3c3688739887eb3d
KHS <hyeoksuk@gmail.com>
2018-02-19 11:36:19 +0900

    불필요한 exception 제거

--- INISGSpringBoot/src/main/java/inisg/boot/HelloController.java
+++ INISGSpringBoot/src/main/java/inisg/boot/HelloController.java
@@ -1,7 +1,5 @@
 package inisg.boot;
 
-import java.io.UnsupportedEncodingException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 	@GetMapping("/hello")
 	@ResponseBody
-	public String hello(@RequestParam(required=true) String name) throws UnsupportedEncodingException {
+	public String hello(@RequestParam(required=true) String name) {
 		return String.format("Hello, %s", name);
 	}
 }
